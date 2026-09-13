427d12fcb9f98b610fa49d57399f09e9f2720fa7
msranj <pradeepp@ymail.com>
2026-09-13 12:03:03 +0530

    renamed files

---
+++ Technical Folder/Notes - Coding/Notes - Powershell/Roadmap - Python.md
@@ -0,0 +1,5 @@
+### Python Learning Resources
+
+- https://github.com/ErdemOzgen/Python-developer-roadmap
+- https://github.com/HalilDeniz/Python30Days
+- 
--- Technical Folder/Notes - Coding/Notes - Powershell/sample-python1.py
+++ Technical Folder/Notes - Coding/Notes - Powershell/sample-python1.py
@@ -1,2 +1,34 @@
 ## check python version
 python --version
+
+#
+### History of python
+- created by Guido Van Rossum in 1991
+- High-Level Programming Language.
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
