b07b18573394813a8b1496c47425861f3d046edc
msranj <pradeepp@ymail.com>
2026-09-13 12:17:02 +0530

    added history of python

--- Technical Folder/Notes - Coding/Notes - Powershell/sample-python1.py
+++ Technical Folder/Notes - Coding/Notes - Powershell/sample-python1.py
@@ -1,13 +1,15 @@
-## check python version
-python --version
-
+# source: 
+    # https://god-level-python.notion.site/0-Start-Here-2b42ff19c7ff4cfd9421df881d3a402c#ffdfd110cfb04fd6a96cc789fda28fd6
 #
 ### History of python
-- created by Guido Van Rossum in 1991
-- High-Level Programming Language.
+
+    # - created by Guido Van Rossum in 1991
+    # - High-Level Programming Language.
 #
 
 
+## check python version
+python --version
 
 
 
